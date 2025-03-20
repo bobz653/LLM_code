0ff2f694056468bcd19813592433757eb9f81546
Bob zhang <bobz653@sina.com>
2025-03-20 14:00:03 +0800

    Update MHA.py

--- MHA.py
+++ MHA.py
@@ -9,8 +9,20 @@ class mha(nn):
     self.head_num = headnum
 
   #x shape:(b,s,h)
-  def forward(self, x):
+  def forward(self, x, attention_mask):
     Q,K,V = self.q_proj(x), self.k_proj(x), self.v_proj(x) 
-    Q = Q.view(b,s,head_num,_).permutue(1,2) #shapd
-    Q = Q.view(b,s,
+    # position encoding
+    # Q = rope(Q) K =rope(K)
+    
+    Q = Q.view(b,s,head_num,_).permutue(1,2) #shape (b,num,s,h)
+    K = K.view(b,s,head_num,_).permutue(1,2) #shape (b,num,s,h)
+    V = V.view(b,s,head_num,_).permutue(1,2) #shape (b,num,s,h)
+
+    weight = Q@K.permute(2,3)/sqrt(hiddensize/headnum)
+    if not attention_mask:
+      weight = weight.fill_mask(attention_mask==0, -1*e10)
+
+    attention = nn.softmax(weight,dim =-1) @ V #(b,num,s,h)
+    attention = attention.transpose(1,2).contiguous().view(b,s,-1)
+    attention = nn.o_proj(attention)
     
